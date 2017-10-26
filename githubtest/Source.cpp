7e69eaa17d9c793c5f747fa223aabeacac769e26
Osama <Osama@197@gmail.com>
2017-10-26 15:33:25 +0900

    added cvtColor

--- githubtest/Source.cpp
+++ githubtest/Source.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "opencv2/highgui/highgui.hpp"
+#include <opencv2/imgproc.hpp>  
 using namespace std;
 using namespace cv;
 int main()
 {
 	Mat img = imread("index.png");
+	cvtColor(img, img, CV_BGR2GRAY);
 	imshow("image",img);
+
 	cv::waitKey();
 
 	return 0;
