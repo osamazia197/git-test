812239c575ee240c0797beb452ba156a5d848903
Osama <Osama@197@gmail.com>
2017-10-26 15:58:48 +0900

    deleted code

--- githubtest/Source.cpp
+++ githubtest/Source.cpp
@@ -5,11 +5,6 @@ using namespace std;
 using namespace cv;
 int main()
 {
-	Mat img = imread("index.png");
-	cvtColor(img, img, CV_BGR2GRAY);
-	imshow("image",img);
-
-	cv::waitKey(10);
 
 	return 0;
 }
